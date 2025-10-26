a5a30fcb195b211a44fc0b6e2a6e3eab2b8e2cd6
Andreas Bok Andersen <bok.chan@gmail.com>
2025-10-26 14:30:20 +0100

    Add role to ecr stack

--- infra/infra/ecr_stack.py
+++ infra/infra/ecr_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import RemovalPolicy, Stack
 from aws_cdk import aws_ecr as ecr
+from aws_cdk import aws_iam as iam
 from constructs import Construct
 
 
@@ -7,7 +8,7 @@ class EcrStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        ecr.Repository(
+        repository = ecr.Repository(
             self,
             "AndreasEcrRepository",
             repository_name="andreas-ecr-repository",
@@ -15,3 +16,32 @@ class EcrStack(Stack):
             removal_policy=RemovalPolicy.RETAIN,
             image_scan_on_push=True,
         )
+
+        # 1. Create the OIDC provider for GitHub Actions
+        github_provider = iam.OpenIdConnectProvider(
+            self,
+            "GitHubOidcProvider",
+            url="https://token.actions.githubusercontent.com",
+            client_ids=["sts.amazonaws.com"],
+        )
+
+        # 2. Create a role for GitHub Actions to assume
+        github_principal = iam.FederatedPrincipal(
+            federated=github_provider.open_id_connect_provider_arn,
+            conditions={
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": "repo:bokchan/iac-task:*"
+                }
+            },
+            assume_role_action="sts:AssumeRoleWithWebIdentity",
+        )
+
+        github_role = iam.Role(
+            self,
+            "GitHubActionRole",
+            assumed_by=github_principal,
+            description="Role for GitHub Actions to push images to ECR",
+        )
+
+        # 3. Grant the role permissions to push/pull to the ECR repository
+        repository.grant_pull_push(github_role)
