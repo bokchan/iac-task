b5a3be3dea6bb4bdd3c63e3541f3bcbb74631f2b
Andreas Bok Andersen <bok.chan@gmail.com>
2025-10-27 10:18:51 +0100

    Trigger workflow

--- infra/config.py
+++ infra/config.py
@@ -7,9 +7,11 @@ class AWSAccountConfig:
     # account_id: str = "146082935119"
     # user_arn: str = "arn:aws:iam::146082935119:user/andreas"
     # user_id: str = "AIDASEAZYWFHQREJANLS6"
+
     # Personal
     account_id: str = "357864525704"
     user_arn: str = "arn:aws:iam::357864525704:user/andreas"
     user_id: str = "AIDAVGUTDJ6EJZ63H3WRQ"
+
     region: str = "eu-central-1"
     log_group_prefix: str = "andreas-applogs"
